310ccb613fb4be76454601ffb9661d114a7670b5
rwr1Mahipal <rwr1.mahipal.sb@gmail.com>
2026-03-06 11:03:45 +0530

    Add files via upload

---
+++ DSA/quick.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void quickSort(int arr[], int left, int right){
+    if(left >= right) return;
+    
+    int pivot = arr[right];
+    int i = left;
+
+    for(int j=left; j<right; j++){
+        if(arr[j] < pivot){
+            swap(arr[i], arr[j]);
+            i++;
+        }
+    }
+    swap(arr[right], arr[i]);
+    quickSort(arr, left, i-1);
+    quickSort(arr, i+1, right);
+}
+
+int main(){
+    int arr[] = {85, 63, 24, 2, 96, 50, 46};
+    int length = sizeof(arr) / sizeof(arr[0]);
+
+    quickSort(arr, 0, length-1);
+
+    for (int i = 0; i < length; i++){
+        cout << arr[i] << " ";
+    }
+}
