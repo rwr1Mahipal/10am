3f6123a979c7d58f626392029a027884ef48f241
rwr1Mahipal <rwr1.mahipal.sb@gmail.com>
2026-04-16 11:06:28 +0530

    Add files via upload

---
+++ JS/arr.js
@@ -0,0 +1,25 @@
+const arr = [10, 50, 40, 100, 80, 30];
+console.log("Original Arr: ", arr);
+// const num = [100,200,300]
+// arr.pop();
+// arr.shift()
+// arr.push(500)
+// arr.unshift(1000)
+// arr.reverse()
+// const arr1 = arr.concat(num)
+
+// console.log(arr.join());
+// console.log(arr.slice(2));
+// arr.splice(0, 3);
+// console.log(arr.length);
+// console.log(arr.indexOf(80));
+
+// const mapFun = arr.map((num) => console.log(num));
+// const arrSort = arr.sort((a, b) => a - b);
+// console.log(arrSort);
+// const numFilter = arr.filter((num) => num > 80);
+// console.log(numFilter);
+
+const res = arr.forEach((num) => console.log(num));
+
+// console.log("Arr: ", arr);
