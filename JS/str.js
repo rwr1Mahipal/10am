c5d66565166cc632eefdb9c1f10bb238e7a68017
rwr1Mahipal <rwr1.mahipal.sb@gmail.com>
2026-04-17 10:51:07 +0530

    Add files via upload

---
+++ JS/str.js
@@ -0,0 +1,18 @@
+const str = "test10@gmail.com";
+const str1 = "jkghjhgjh"
+// const test = str.length;
+// const test = str.toUpperCase();
+// const test = str.toLowerCase();
+// const test = str.charAt("2");
+// const test = str.includes("125");
+// const test = str.trim();
+// const test = str.slice(1,4);
+// const test = str.split();
+// const test = str.substring(0,4) + "****";
+// const str2 = str.concat(str1)
+// const test = str.replace("@", "%");
+
+
+
+
+console.log(test);
