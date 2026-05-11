57228c7301b89891af6ec8e07533198098c851c2
MAHIPAL SINGH <rwr1.mahipal.sb@gmail.com>
2026-05-11 10:52:53 +0530

    Add files via upload

---
+++ JS/event.js
@@ -0,0 +1,31 @@
+// const btn = document.getElementById("btn");
+
+// btn.addEventListener("click", () => {
+//   alert("Hello");
+// });
+
+// const text = document.querySelector(".text")
+// const text = document.querySelectorAll(".text");
+
+// text.forEach((item)=>{
+//     item.innerHTML = "Hello"
+// })
+
+const techStack = [
+  { id: 1, tech: "JavaScript", version: 7 },
+  { id: 2, tech: "HTML", version: 5 },
+  { id: 3, tech: "ReactJs", version: 18 },
+  { id: 4, tech: "CSS", version: 3 },
+];
+
+const btn = document.getElementById("btn");
+const box = document.getElementById("box");
+
+btn.addEventListener("click", () => {
+  box.innerHTML = "";
+  techStack.forEach((tech) => {
+    const p = document.createElement("p");
+    p.innerHTML = `${tech.tech}`;
+    box.appendChild(p);
+  });
+});
